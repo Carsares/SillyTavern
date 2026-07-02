df42e94fe5536dab2f89f3ca8fef0bd90cccb981
molei <molei@wepie.com>
2026-07-03 01:54:09 +0800

    refactor: split modern route context helpers

---
+++ public/modern/shell/route-context-chat.js
@@ -0,0 +1,143 @@
+export function createChatContextRouteContext(chatContext) {
+    const {
+        isGroupChatMode,
+        getChatModeLabel,
+        getSelectedChatEntity,
+        getChatContextKey,
+        getChatEntityName,
+        getChatEntityAvatarUrl,
+        getChatEntityFallbackIcon,
+        getChatEntityEmptyTitle,
+        getChatEntityEmptyDescription,
+        getChatEntityListEmptyText,
+        getSelectedChatList,
+        getChatId,
+        getChatMessageCount,
+        getVisibleChatList,
+        getSelectedChatMessages,
+        getCurrentMessageLimit,
+        getChatCacheKey,
+        clearChatSearch,
+        searchSelectedChats,
+        loadChatMessages,
+        prepareChatForSelectedContext,
+        getCurrentDraftKey,
+        getCurrentDraft,
+        setCurrentDraft,
+        increaseCurrentMessageLimit,
+        closeChatSidebarForMobileSelection,
+    } = chatContext;
+
+    return {
+        isGroupChatMode,
+        getChatModeLabel,
+        getSelectedChatEntity,
+        getChatContextKey,
+        getChatEntityName,
+        getChatEntityAvatarUrl,
+        getChatEntityFallbackIcon,
+        getChatEntityEmptyTitle,
+        getChatEntityEmptyDescription,
+        getChatEntityListEmptyText,
+        getSelectedChatList,
+        getChatId,
+        getChatMessageCount,
+        getVisibleChatList,
+        getSelectedChatMessages,
+        getCurrentMessageLimit,
+        getChatCacheKey,
+        clearChatSearch,
+        searchSelectedChats,
+        loadChatMessages,
+        prepareChatForSelectedContext,
+        getCurrentDraftKey,
+        getCurrentDraft,
+        setCurrentDraft,
+        increaseCurrentMessageLimit,
+        closeChatSidebarForMobileSelection,
+    };
+}
+
+export function createChatFileRouteContext(chatFiles) {
+    const {
+        startNewModernChat,
+        beginModernChatRename,
+        cancelModernChatRename,
+        saveModernChatRename,
+        beginModernChatDelete,
+        cancelModernChatDelete,
+        confirmModernChatDelete,
+        importModernChatFiles,
+        exportModernChat,
+        loadChatBackups,
+        toggleChatBackups,
+        viewChatBackup,
+        restoreChatBackup,
+        beginChatBackupDelete,
+        cancelChatBackupDelete,
+        confirmChatBackupDelete,
+    } = chatFiles;
+
+    return {
+        startNewModernChat,
+        beginModernChatRename,
+        cancelModernChatRename,
+        saveModernChatRename,
+        beginModernChatDelete,
+        cancelModernChatDelete,
+        confirmModernChatDelete,
+        importModernChatFiles,
+        exportModernChat,
+        loadChatBackups,
+        toggleChatBackups,
+        viewChatBackup,
+        restoreChatBackup,
+        beginChatBackupDelete,
+        cancelChatBackupDelete,
+        confirmChatBackupDelete,
+    };
+}
+
+export function createChatGenerationRouteContext(chatGeneration) {
+    const {
+        checkLegacyGenerationEngine,
+        sendModernMessage,
+        stopModernGeneration,
+        regenerateModernReply,
+        continueModernReply,
+        swipeModernMessage,
+    } = chatGeneration;
+
+    return {
+        checkLegacyGenerationEngine,
+        sendModernMessage,
+        stopModernGeneration,
+        regenerateModernReply,
+        continueModernReply,
+        swipeModernMessage,
+    };
+}
+
+export function createChatMessageRouteContext(chatMessages) {
+    const {
+        copyModernMessage,
+        deleteModernMessage,
+        beginModernMessageDelete,
+        cancelModernMessageDelete,
+        confirmModernMessageDelete,
+        beginModernMessageEdit,
+        cancelModernMessageEdit,
+        saveModernMessageEdit,
+    } = chatMessages;
+
+    return {
+        copyModernMessage,
+        deleteModernMessage,
+        beginModernMessageDelete,
+        cancelModernMessageDelete,
+        confirmModernMessageDelete,
+        beginModernMessageEdit,
+        cancelModernMessageEdit,
+        saveModernMessageEdit,
+    };
+}
---
+++ public/modern/shell/route-context-shared.js
@@ -0,0 +1,135 @@
+export function createComponentRouteContext(components) {
+    const {
+        metricCard,
+        pageHead,
+        renderCharacterRow,
+        renderEmptyState,
+        renderGroupRow,
+        renderInlineEmpty,
+        renderKeyValue,
+    } = components;
+
+    return {
+        metricCard,
+        pageHead,
+        renderCharacterRow,
+        renderEmptyState,
+        renderGroupRow,
+        renderInlineEmpty,
+        renderKeyValue,
+    };
+}
+
+export function createActivityRouteContext(activity) {
+    const {
+        getActivityEntries,
+        getActivitySummary,
+        recreateStats,
+    } = activity;
+
+    return {
+        getActivityEntries,
+        getActivitySummary,
+        recreateStats,
+    };
+}
+
+export function createApiRouteContext(api) {
+    const {
+        getOaiSettings,
+        getSelectedApiMain,
+        getChatCompletionModel,
+        getApiModelSuggestions,
+        getApiSourceUiState,
+        getNumberSetting,
+        getSecretStateForSource,
+        testApiConnection,
+        setApiModelSuggestion,
+        saveApiConnectionFromForm,
+        updateApiSourceFields,
+        updateTextCompletionTypeFields,
+    } = api;
+
+    return {
+        getSelectedApiMain,
+        getChatCompletionModel,
+        getApiModelSuggestions,
+        getApiSourceUiState,
+        getNumberSetting,
+        getSecretStateForSource,
+        getOaiSettings,
+        testApiConnection,
+        setApiModelSuggestion,
+        saveApiConnectionFromForm,
+        updateApiSourceFields,
+        updateTextCompletionTypeFields,
+    };
+}
+
+export function createPresetRouteContext(presets) {
+    const {
+        getPresetCount,
+        getPresetGroups,
+        getVisiblePresetGroups,
+        getSelectedPresetRecord,
+        getPresetEditorText,
+        saveOpenAiPresetFromForm,
+        selectPreset,
+        savePresetJsonFromEditor,
+        useOpenAiPreset,
+        duplicatePreset,
+        exportPreset,
+        restorePreset,
+        beginPresetDelete,
+        cancelPresetDelete,
+        confirmPresetDelete,
+        updatePresetEditorText,
+        importPresetFile,
+    } = presets;
+
+    return {
+        getPresetCount,
+        getPresetGroups,
+        getVisiblePresetGroups,
+        getSelectedPresetRecord,
+        getPresetEditorText,
+        saveOpenAiPresetFromForm,
+        selectPreset,
+        savePresetJsonFromEditor,
+        useOpenAiPreset,
+        duplicatePreset,
+        exportPreset,
+        restorePreset,
+        beginPresetDelete,
+        cancelPresetDelete,
+        confirmPresetDelete,
+        updatePresetEditorText,
+        importPresetFile,
+    };
+}
+
+export function createSettingsRouteContext(settings) {
+    const {
+        getRequestCompressionSettings,
+        loadSettingsSnapshots,
+        createSettingsSnapshot,
+        saveModernPreferencesFromForm,
+        saveRequestCompressionFromForm,
+        previewSettingsSnapshot,
+        beginSettingsSnapshotRestore,
+        cancelSettingsSnapshotRestore,
+        confirmSettingsSnapshotRestore,
+    } = settings;
+
+    return {
+        getRequestCompressionSettings,
+        loadSettingsSnapshots,
+        createSettingsSnapshot,
+        saveModernPreferencesFromForm,
+        saveRequestCompressionFromForm,
+        previewSettingsSnapshot,
+        beginSettingsSnapshotRestore,
+        cancelSettingsSnapshotRestore,
+        confirmSettingsSnapshotRestore,
+    };
+}
